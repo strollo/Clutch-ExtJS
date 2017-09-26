683bb435ce41a564675471fd2fa34b6b13d71a04
Daniele Strollo <daniele.strollo@gmail.com>
2017-09-26 13:00:09 +0200

    Added Proxy settings page

--- app/view/settings/ProxySettings.js
+++ app/view/settings/ProxySettings.js
@@ -1,75 +1,83 @@
+Ext.define('Clutch.model.HashData', {
+    extend : 'Ext.data.Model',
+    fields : [{
+        name : 'label',
+        type : 'string'
+    },{
+		name : 'value',
+		type : 'number'
+	}]
+});
+
 Ext.define('Clutch.view.settings.ProxySettings', {
-	
-    extend : 'Clutch.view.settings.SettingsCardBase',
-    title : 'Proxy',
-
-    config : {
-        fields : ['proxy-server-enabled', 'proxy-type', 'proxy-server', 'proxy-port', 'proxy-authentication-required', 'proxy-authentication', 'proxy-username']
-    },
-
-    alias : 'widget.proxysettings',
-
-    items : [{
-        xtype : 'fieldset',
-        title : 'Bandwidth',
-        items : [{
+	extend : 'Clutch.view.settings.SettingsCardBase',
+	requires : ['Clutch.model.HashData'],
+	title : 'Proxy',
+	config : {
+		fields : ['proxy-server-enabled', 'proxy-type', 'proxy-server', 'proxy-port', 'proxy-authentication-required', 'proxy-authentication', 'proxy-username']
+	},
+	alias : 'widget.proxysettings',
+	items : [{
+		xtype : 'fieldset',
+		title : 'Proxy',
+		items : [
+		{
+			fieldLabel : 'Proxy enabled',
+			allowBla1nk : false,
+			xtype : 'checkbox',
+			name : 'proxy-server-enabled'
+		},
+		{
+			fieldLabel : 'Proxy Type',			
+			xtype: 'combobox',
+			displayField: 'label',
+			valueField: 'value',
+			store: Ext.create('Ext.data.Store', {
+				model: 'Clutch.model.HashData',
+				type : 'json',
+				proxy : {
+					type : 'memory'
+				},					
+				data: [
+					{label: 'HTTP', value: 0},
+					{label: 'SOCKS4', value: 1},
+					{label: 'SOCKS5', value: 2}
+				]						
+			}),
+			name : 'proxy-type'
+		},
+		{
+			fieldLabel : 'Proxy server',
+			allowBlank : false,
+			xtype : 'textfield',
+			name : 'proxy-server'
+		},
+		{
+			fieldLabel : 'Proxy port',
+			allowBlank : false,
+			xtype : 'numberfield',
+			name : 'proxy-port'
+		},
+		{
             xtype : 'fieldset',
-            title : 'Normal Mode',
-            items : [
+            title : 'Authentication',
+            items : [{			
+				boxLabel : 'Authentication required',
+				xtype : 'checkbox',
+				name : 'proxy-authentication-required'
+			},
 			{
-                fieldLabel : 'Proxy enabled',
-                allowBlank : false,
-                xtype : 'checkbox',
-                name : 'proxy-server-enabled'
-            },
+				fieldLabel : 'Proxy Authentication',
+				allowBlank : false,
+				xtype : 'textfield',
+				name : 'proxy-authentication'
+			}, 
 			{
-				fieldLabel : 'Proxy Type',
-				xtype: 'combobox',
-				displayField: 'label',
-				valueField: 'value',
-				store: Ext.create('Ext.data.Store', {
-					type : 'json',
-					proxy : {
-						type : 'memory'
-					},
-					data: [
-						{label: 'HTTP', value: 0},
-						{label: 'SOCKS4', value: 1},
-						{label: 'SOCKS5', value: 2}
-					]						
-				}),
-				name : 'proxy-type'
-			},			
-			{
-                fieldLabel : 'Proxy server',
-                allowBlank : false,
-                xtype : 'textfield',
-                name : 'proxy-server'
-            },
-			{
-                fieldLabel : 'Proxy port',
-                allowBlank : false,
-                xtype : 'numberfield',
-                name : 'proxy-port'
-            },
-			{
-                boxLabel : 'Authentication required',
-                xtype : 'checkbox',
-                name : 'proxy-authentication-required'
-            },
-			{
-                fieldLabel : 'Proxy Authentication',
-                allowBlank : false,
-                xtype : 'textfield',
-                name : 'proxy-authentication'
-            }, 
-			{
-                fieldLabel : 'Proxy Username',
-                allowBlank : false,
-                xtype : 'textfield',
-                name : 'proxy-username'
-            }
-          ]
+				fieldLabel : 'Proxy Username',
+				allowBlank : false,
+				xtype : 'textfield',
+				name : 'proxy-username'
+			}]
 		}]
 	}]
 });
